0292adc4bbc81b5824b57ab3354f0daeef860f0f
fatemehmollaei <fatemeh.mollaei.m@gmail.com>
2021-05-28 23:46:43 +0430

    done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,40 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter} from "react-router-dom";
+import AssetsReducer from './store/reducers/Assets';
+import CurrencyReducer from './store/reducers/Currency';
+// import "./assets/css/bootstrap-rtl.min.css"
+// import "./assets/css/bootstrap.min.css"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const rootReducer = combineReducers({
+  Assets : AssetsReducer,
+  Currency : CurrencyReducer
+})
+
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
+
+const app = (
+  <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </Provider>
+)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,7 +45,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
