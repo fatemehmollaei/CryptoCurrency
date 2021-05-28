5ecf06cbb403e709723a5184fb21195ff0a371b8
fatemehmollaei <fatemeh.mollaei.m@gmail.com>
2021-05-28 23:47:42 +0430

    done

--- src/hoc/Footer/Footer.js
+++ src/hoc/Footer/Footer.js
@@ -1,24 +1,45 @@
 import classes from './Footer.module.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Wrapper from '../Wrapper/Wrapper'
 import Button from '../../component/UI/Button/Button'
 import setting from '../../assets/image/setting.svg'
 import chart from '../../assets/image/chart.svg'
 import main from '../../assets/image/main.svg'
 
-const footer = ()=>  {
+const Footer = ()=>  {
+ 
+  const [activeState, setActivated] = useState([true,false,false]);
+    const active = (state)=>{
+      
+    switch(state){
+      case 1 : {setActivated([true,false,false]);
+                break}
+                  
+      case 2 : {setActivated([false,true,false]);
+                break}
+
+      case 3 : {setActivated([false,false,true]);
+               break}
+
+       default:{setActivated([true,false,false]);
+        break}   
+      }
+     
+      console.log(activeState)
+  }
+   
         return (
             <Wrapper>
                 <footer className={classes.footer} >
                   <div className={classes.footerButton}>
 
-                        <Button link="/setting" active="true" background={setting}/>
-                        <Button link="/currency" active="false" background={chart}/>
-                        <Button link="/" active="false" background={main}/>
+                        <Button link="/" active={activeState[0]} background={setting} onclick={()=>active(1)}/>
+                        <Button link="/" active={activeState[1]} background={chart} onclick={()=>active(2)}/>
+                        <Button link="/" active={activeState[2]} background={main} onclick={()=>active(3)}/>
                         
                   </div>
                 </footer>
             </Wrapper>
         )
  }
-export default footer
+export default Footer
