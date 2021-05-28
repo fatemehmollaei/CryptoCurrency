8e1bfd39ef72650d54e7a3bcec679f960b002341
fatemehmollaei <fatemeh.mollaei.m@gmail.com>
2021-05-28 23:47:28 +0430

    done

--- src/store/actions/Currency.js
+++ src/store/actions/Currency.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes'
 import axios from '../../axios-orders'
+import  {Daily,Monthly,Yearly} from '../../calc-history'
 
 export const currencyStart = () => {
     return {
@@ -14,6 +15,27 @@ export const  currencySuccess = (data) => {
     }
 }
 
+export const  currencyDSuccess = (data) => {
+    return {
+        type:actionTypes.CURRENCYD_SUCCESS,
+        currency:data.currency
+    }
+}
+
+export const  currencyMSuccess = (data) => {
+    return {
+        type:actionTypes.CURRENCYM_SUCCESS,
+        currency:data.currency
+    }
+}
+
+export const  currencyYSuccess = (data) => {
+    return {
+        type:actionTypes.CURRENCYY_SUCCESS,
+        currency:data.currency
+    }
+}
+
 export const currencyFail = (error) => {
     return {
         type:actionTypes.CURRENCY_FAIL,
@@ -21,18 +43,15 @@ export const currencyFail = (error) => {
     }
 }
 
-
-
-export const currencyDay = (id) => {
+export const currency = (id) => {
 
     return dispatch => {
 
         dispatch(currencyStart())
  
         axios.get('v2/assets/'+id)
-
-            .then(response => {
            
+            .then(response => {
                 dispatch(currencySuccess({
                     currency:response.data.data
                 }))
@@ -45,17 +64,45 @@ export const currencyDay = (id) => {
     }
 }
 
-export const currencyMonth = (id) => {
+export const currencyDay = (id) => {
+  
+    const history = Daily()
+    const url = 'v2/assets/'+id+'/history?interval=h1&start='+history.start+'&end='+history.end
 
     return dispatch => {
 
         dispatch(currencyStart())
  
-        axios.get('v2/assets/'+id+'/history?interval=d1')
+        axios.get(url)
 
             .then(response => {
-           
-                dispatch(currencySuccess({
+              
+                dispatch(currencyDSuccess({
+                    currency:response.data.data
+                }))
+
+            })
+            .catch(err => {
+                dispatch(currencyFail(err.message))
+            })
+
+    }
+}
+
+export const currencyMonth = (id) => {
+   
+    const history = Monthly()
+    const url = 'v2/assets/'+id+'/history?interval=d1&start='+history.start+'&end='+history.end
+
+    return dispatch => {
+
+        dispatch(currencyStart())
+ 
+        axios.get(url)
+
+            .then(response => {
+              
+                dispatch(currencyMSuccess({
                     currency:response.data.data
                 }))
 
@@ -69,15 +116,17 @@ export const currencyMonth = (id) => {
 
 export const currencyYear = (id) => {
 
+    const history = Yearly()
+    const url = 'v2/assets/'+id+'/history?interval=d1&start='+history.start+'&end='+history.end
+
     return dispatch => {
 
         dispatch(currencyStart())
  
-        axios.get('v2/assets/'+id+'/history?interval=d1')
+        axios.get(url)
 
             .then(response => {
-           
-                dispatch(currencySuccess({
+                dispatch(currencyYSuccess({
                     currency:response.data.data
                 }))
 
--- src/store/reducers/Currency.js
+++ src/store/reducers/Currency.js
@@ -3,6 +3,9 @@ import { updateObject } from '../utiliy'
 
 const initialState = {
     currency : null,
+    currencyDay : null,
+    currencyMonth : null,
+    currencyYear : null,
     error  : null,
     loading: false
 }
@@ -18,16 +21,33 @@ const currencySuccess = (state, action) => {
         currency:action.currency
     })
 }
+const currencyDSuccess = (state, action) => {
+    return updateObject(state, {
+        currencyDay:action.currency
+    })
+}
+const currencyMSuccess = (state, action) => {
+    return updateObject(state, {
+        currencyMonth:action.currency
+    })
+}
+const currencyYSuccess = (state, action) => {
+    return updateObject(state, {
+        currencyYear:action.currency
+    })
+}
 
 const currencyFail = (state, action) => {
     return updateObject(state, { error: action.error, loading: false })
 }
   
 const searchReducer = (state = initialState, action) => {
-
     switch (action.type) {
         case actionTypes.CURRENCY_START: return currencyStart(state, action);
         case actionTypes.CURRENCY_SUCCESS: return currencySuccess(state, action)
+        case actionTypes.CURRENCYD_SUCCESS: return currencyDSuccess(state, action)
+        case actionTypes.CURRENCYM_SUCCESS: return currencyMSuccess(state, action)
+        case actionTypes.CURRENCYY_SUCCESS: return currencyYSuccess(state, action)
         case actionTypes.CURRENCY_FAIL: return currencyFail(state, action)
         default: return state
     }
