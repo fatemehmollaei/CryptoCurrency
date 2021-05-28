386dec0f4b22a87ebbef9248eedf5d74ed005b49
fatemehmollaei <fatemeh.mollaei.m@gmail.com>
2021-05-28 23:46:20 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import AssetsBuilder from './container/AssetsBuilder/AssetsBuilder'
+import Main from './hoc/Main/Main';
+import CurrencyBuilder from './container/CurrencyBuilder/CurrencyBuilder'
+class App extends Component {
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/"  exact component={AssetsBuilder} />
+        <Route path="/currency/:id"   component={CurrencyBuilder} />
+      </Switch>
+    )
+    return (
+      <div className="App">
+        <Main >
+          {routes}
+        </Main>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
